0d6898b0f022b6a8bf5c60f5b7a6213be039ab39
xmilygit <xmilygame@163.com>
2017-03-08 12:07:19 +0800

    添加未还书查询代码

--- routes/library.js
+++ routes/library.js
@@ -30,8 +30,30 @@ router.get('/txm',function(req,res,next){
 })
 
 router.post('/unreturnBook',function(req,res,next){
-    console.log(req["pagesize"]+"==="+req["pagenum"]+"==="+req["keyword"]+"==="+req["datarang"]);
+    let pagesize=req.body.pagesize;
+    let pagenum=req.body.pagenum;
+    let beginnum=pagesize*pagenum;
+    let keyword=req.body.keyword;
+    let daterange1=req.body.daterange.substring(0,10).replace(/-/g,'');
+    let daterange2=req.body.daterange.substring(13,23).replace(/-/g,'');
+    //console.log(daterange1+"***"+daterange2);
+
+    sql.connect(config).then(function(){
+        console.log('mssql连接成功，正在处理未还书数据');
+        new sql.Request()
+        .query("with temp as(select *,row_number() over(order by hnyr) as id from v_borrow where hsbz=0) select top "+pagesize+" * from temp where id between "+beginnum+" and "+beginnum+pagesize).then(function(recordset){
+            console.log('成功获得数据');
+        }).catch(function(err){
+            console.log("获取数据失败："+err);
+        })
+    })
+
+
+    
+
     res.end();
+
+    
 })
 
 
