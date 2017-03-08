0a0015e9c2470bd599cf53103b23494972f8cf4e
xmilygit <xmilygame@163.com>
2017-03-09 01:13:02 +0800

    unreturnbook请求代码

--- routes/library.js
+++ routes/library.js
@@ -11,49 +11,67 @@ var config = {
         tdsVersion: '7_1'
     }
 }
-
-router.get('/txm',function(req,res,next){
-    sql.connect(config).then(function(){
+router.get('/test',function(req,res,next){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json({type:"error",message:"aaaaaaaa"});
+})
+router.get('/txm', function (req, res, next) {
+    sql.connect(config).then(function () {
         console.log('mssql连接成功');
-        let sqlreq=new sql.Request();
-        sqlreq.input('txm',sql.Char,req.query['txm']);
-        sqlreq.query('select * from ts where txm=@txm').then(function(recordset){
+        let sqlreq = new sql.Request();
+        sqlreq.input('txm', sql.Char, req.query['txm']);
+        sqlreq.query('select * from ts where txm=@txm').then(function (recordset) {
             console.log(recordset);
             res.json(recordset);
             //res.end(json(recordset));
-        }).catch(function(err){
+        }).catch(function (err) {
             console.log(err);
         })
-    }).catch(function(err){
-        console.log('数据连接出错：'+err);
+    }).catch(function (err) {
+        console.log('数据连接出错：' + err);
     })
 })
 
-router.post('/unreturnBook',function(req,res,next){
-    let pagesize=req.body.pagesize;
-    let pagenum=req.body.pagenum;
-    let beginnum=pagesize*pagenum;
-    let keyword=req.body.keyword;
-    let daterange1=req.body.daterange.substring(0,10).replace(/-/g,'');
-    let daterange2=req.body.daterange.substring(13,23).replace(/-/g,'');
+router.post('/unreturnBook', function (req, res, next) {
+    let currentpage=parseInt(req.body.currentpage)-1;
+    let pagesize = parseInt(req.body.pagesize);
+    //let pagenum = parseInt(req.body.pagenum);
+    let beginnum = parseInt(pagesize * currentpage);
+    let endnum=beginnum+pagesize;
+    let keyword = req.body.keyword;
+    let daterange1 = req.body.daterange.substring(0, 10).replace(/-/g, '');
+    let daterange2 = req.body.daterange.substring(13, 23).replace(/-/g, '');
     //console.log(daterange1+"***"+daterange2);
-
-    sql.connect(config).then(function(){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    sql.connect(config).then(function () {
         console.log('mssql连接成功，正在处理未还书数据');
-        new sql.Request()
-        .query("with temp as(select *,row_number() over(order by hnyr) as id from v_borrow where hsbz=0) select top "+pagesize+" * from temp where id between "+beginnum+" and "+beginnum+pagesize).then(function(recordset){
-            console.log('成功获得数据');
-        }).catch(function(err){
-            console.log("获取数据失败："+err);
-        })
+        if (daterange1.trim().length <= 0) {
+            let sqlstr="with temp as(select *,row_number() over(order by hnyr) as id from v_borrow where hsbz=0 and (dzxm like '%" + keyword + "%' or sm like '%" + keyword + "%')) select top " + pagesize + " * from temp where id between " + beginnum + " and " + endnum
+            new sql.Request()
+                .query(sqlstr).then(function (recordset) {
+                    console.log('成功获得数据');
+                    res.json(recordset);
+                }).catch(function (err) {
+                    console.log("获取数据失败：" + err);
+                    res.json({ type: 'error', message: '获取数据失败：' + err });
+                })
+        } else {
+            let sqlstr="with temp as(select *,row_number() over(order by hnyr) as id from v_borrow where hsbz=0 and hnyr>='" + daterange1 + "' and hnyr<='" + daterange2 + "' and (dzxm like '%" + keyword + "%' or sm like '%" + keyword + "%')) select top " + pagesize + " * from temp where id between " + beginnum + " and " + endnum;
+            console.log(sqlstr)
+            new sql.Request()
+                .query(sqlstr).then(function (recordset) {
+                    console.log('成功获得数据');
+                    res.json(recordset);
+                }).catch(function (err) {
+                    console.log("获取数据失败：" + err);
+                    res.json({ type: 'error', message: '获取数据失败：' + err });
+                })
+
+        }
+    }).catch(function (err) {
+        console.log('数据库连接出错：' + err)
+        res.json({ type: 'error', message: '数据库连接出错：' + err });
     })
-
-
-    
-
-    res.end();
-
-    
 })
 
 
@@ -61,21 +79,21 @@ router.get('/', function (req, res, next) {
     sql.connect(config).then(function () {
         console.log('mssql连接成功');
         new sql.Request()
-        //.input('topnum',sql.Int,req.query["topnum"])
-        .query('select top '+req.query["topnum"]+' * from ts order by txm desc').then(function(recordset){
-            console.log(recordset);
-        }).catch(function(err){
-            console.log(err);
-        })
+            //.input('topnum',sql.Int,req.query["topnum"])
+            .query('select top ' + req.query["topnum"] + ' * from ts order by txm desc').then(function (recordset) {
+                console.log(recordset);
+            }).catch(function (err) {
+                console.log(err);
+            })
 
     }).catch(function (err) {
         console.log(err);
     })
-    res.render('Library/index',{title:'图书',qm:'aaaaaaa'});
+    res.render('Library/index', { title: '图书', qm: 'aaaaaaa' });
 })
 
-module.exports=router;
+module.exports = router;
 
 
-//with temp as(select *,row_number() over(order by hnyr) as id from v_borrow where hsbz=0)
+//with temp as(select *,row_number() over(order by hnyr) as id from v_borrow where hsbz=0 and hnyr>='' and hnyr<='' and (dzxm like '%%' or sm like '%%')))
 //select top 10 * from temp where id between 230 and 240
