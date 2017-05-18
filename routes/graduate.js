38f7bd4547c243e11b2624296adb91772e33d258
xmilygit <xmilygame@163.com>
2017-05-18 23:23:45 +0800

    useropneid

--- routes/graduate.js
+++ routes/graduate.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var async = require('async')
-var request=require('request');
+var request = require('request');
 var router = express.Router();
 
 
@@ -11,29 +11,40 @@ var myauth = {
     token: 'xmilyhh'
 };
 
-router.get('/',function(req,res,next){
-    let code=req.query['code'];
-    let url="https://api.weixin.qq.com/sns/oauth2/access_token?appid="+myauth.appid+"&secret="+myauth.appsecret+"&code="+code+"&grant_type=authorization_code "
-let useropenid='';
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log('打印结果：'+JSON.parse(body).openid) // 打印google首页
-    console.log(body)
-    useropenid=JSON.parse(body).openid;
-    res.render('graduate/index',{
-        title:'桂林市凤集小学--毕业生信息管理系统',
-        layout:'f7layouts',
-        useropenid:useropenid,
-        code:req.query['code']
+router.get('/', function (req, res, next) {
+    let code = req.query['code'];
+    let useopenid = fun.getUseOpenId(code);
+    console.log(useopenid)
+    res.render('graduate/index', {
+        title: '桂林市凤集小学--毕业生信息管理系统',
+        layout: 'f7layouts',
+        useropenid: useropenid,
+        code: req.query['code']
     });
-  }else{
-      console.log('获取用户openid出错：'+error);
-      res.end();
-  }
-})    
-    
-    
-});
+})
+
+var fun = {
+    "getUseOpenId": function (code) {
+        async.series([
+            function (callback) {
+                let url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + myauth.appid + "&secret=" + myauth.appsecret + "&code=" + code + "&grant_type=authorization_code "
+                request(url, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        //console.log('打印结果：' + JSON.parse(body).openid) 
+                        //console.log(body)
+                        callback(null, JSON.parse(body).openid)
+                    } else {
+                        callback(null, "error");
+                    }
+                })
+            }
+        ], function (err, result) {
+            if (result[0] != "error")
+                return result[0];
+        })
+
+    }
+}
 
 
-module.exports=router;
+module.exports = router;
