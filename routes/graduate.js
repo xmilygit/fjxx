2e34fb1c49aeb13871307e416eb6b1a1f3e1e092
xmilygit <xmilygame@163.com>
2017-05-21 23:46:33 +0800

    useopenid获取1

--- routes/graduate.js
+++ routes/graduate.js
@@ -13,35 +13,48 @@ var myauth = {
 
 router.get('/', function (req, res, next) {
     let code = req.query['code'];
-    let useopenid = fun.getUseOpenId(code);
-    console.log(useopenid)
-    res.render('graduate/index', {
-        title: '桂林市凤集小学--毕业生信息管理系统',
-        layout: 'f7layouts',
-        useropenid: useropenid,
-        code: req.query['code']
-    });
+    async.series([
+        function (callback) {
+            fun.getUseOpenId(code, callback);
+        }
+    ], function (error, result) {
+        if (result[0] != "error") {
+            res.render('graduate/index', {
+                title: '桂林市凤集小学--毕业生信息管理系统',
+                layout: 'f7layouts',
+                useropenid: result[0],
+                code: req.query['code']
+            });
+        } else {
+            console.log(error)
+        }
+    })
+
+
+
+
 })
 
 var fun = {
-    "getUseOpenId": function (code) {
-        async.series([
-            function (callback) {
-                let url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + myauth.appid + "&secret=" + myauth.appsecret + "&code=" + code + "&grant_type=authorization_code "
-                request(url, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        //console.log('打印结果：' + JSON.parse(body).openid) 
-                        //console.log(body)
-                        callback(null, JSON.parse(body).openid)
-                    } else {
-                        callback(null, "error");
-                    }
-                })
+    "getUseOpenId": function (code, callback) {
+        //async.series([
+        //function (callback) {
+        let url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + myauth.appid + "&secret=" + myauth.appsecret + "&code=" + code + "&grant_type=authorization_code "
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //console.log('打印结果：' + JSON.parse(body).openid) 
+                //console.log(body)                    
+                callback(null, JSON.parse(body).openid);
+            } else {
+                console.log("获取useropenid时出错：" + error)
+                callback(null, "error");
             }
-        ], function (err, result) {
-            if (result[0] != "error")
-                return result[0];
         })
+        //}
+        //], function (err, result) {
+        //    if (result[0] != "error")
+        //        return result[0];
+        //})
 
     }
 }
