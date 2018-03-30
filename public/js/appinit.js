4647a907e8c384917e872dfb9b4a37ca107debe4
xmilygit <xmilygame@163.com>
2018-03-30 11:55:14 +0800

    临时修改了AJAX服务器地址

--- public/js/appinit.js
+++ public/js/appinit.js
@@ -1,5 +1,5 @@
 //定义AJAX服务器地址
-//var svrUrl = "http://fjxx.tunnel.echomod.cn";
-var svrUrl="http://xmilyhome.tunnel.echomod.cn";
+var svrUrl = "http://fjxx.tunnel.echomod.cn";
+//var svrUrl="http://xmilyhome.tunnel.echomod.cn";
 //var svrUrl = "http://127.0.0.1:3000";
 //var svrUrl='http://hmfj.ngrok.cc';
