0988b5e75517d27663abfdeda16267e7b98c5b9c
xmilygit <xmilygame@163.com>
2018-04-09 23:11:54 +0800

    20180409

--- public/js/f7/js/my-app-graduateinfoinput.js
+++ public/js/f7/js/my-app-graduateinfoinput.js
@@ -1,53 +1,3 @@
-/*
-var lv1 = "[1-9][0-9]0{10}"
-var lv2init = "11([0-9][1-9]|[1-9][0-9])0{8}"
-var lv3init = "1101([0-9][1-9]|[1-9][0-9])0{6}"
-var temppk = [];
-var currentPid="";
-function returnLocationArray(regex) {
-    var lnames = $.Enumerable.From(locationcode)
-        .Where("x=>x.code.match(/" + regex + "/)")
-        .OrderBy("x=>x.code")
-        .Select("x=>x.locationname")
-        .ToArray();
-    var lcodes = $.Enumerable.From(locationcode)
-        .Where("x=>x.code.match(/" + regex + "/)")
-        .OrderBy("x=>x.code")
-        .Select("x=>x.code")
-        .ToArray();
-    return { names: lnames, codes: lcodes };
-
-}
-*/
-//学生身份证设置为禁用后，该功能取消
-/*
-$("#pid").on('blur', function (obj) {
-    var newVal = [];
-    var targetVal = $(this).val()
-    var c1 = targetVal.substr(0, 2);
-    var c2 = targetVal.substr(0, 4);
-    var c3 = targetVal.substr(0, 6);
-    newVal = [c1 + '0000000000', c2 + '00000000', c3 + '000000'];
-    if ($("#locationcode1").val().length == 0) {
-        locationpick1.setValue(newVal)
-        temppk[0] = newVal;
-    }
-    if ($("#locationcode2").val().length == 0) {
-        locationpick2.setValue(newVal)
-        temppk[1] = newVal;
-    }
-    if ($("#locationcode3").val().length == 0) {
-        locationpick3.setValue(newVal)
-        temppk[2] = newVal;
-    }
-    if ($("#locationcode4").val().length == 0) {
-        locationpick4.setValue(newVal)
-        temppk[3] = newVal;
-    }
-})
-*/
-
-
 
 $(function() {
         //获取JS-SDK配置信息
@@ -192,12 +142,13 @@ var locationpick2 = myApp.picker({
         return displayValues[0] + displayValues[1] + displayValues[2]
     },
     toolbarCloseText: '确定',
-    //onOpen: function (picker) {
+    onOpen: function (picker) {
     //    if (temppk[0]) {
     //        picker.setValue(temppk[0])
     //        temppk[0] = null
     //    }
-    //},
+    picker.setValue(defaultlocation);
+    },
     cols: [{
             width: 160,
             textAlign: 'left',
@@ -248,12 +199,13 @@ var locationpick3 = myApp.picker({
         return displayValues[0] + displayValues[1] + displayValues[2]
     },
     toolbarCloseText: '确定',
-    //onOpen: function (picker) {
+    onOpen: function (picker) {
     //    if (temppk[0]) {
     //        picker.setValue(temppk[0])
     //        temppk[0] = null
     //    }
-    //},
+    picker.setValue(defaultlocation);
+    },
     cols: [{
             width: 160,
             textAlign: 'left',
@@ -304,12 +256,13 @@ var locationpick4 = myApp.picker({
         return displayValues[0] + displayValues[1] + displayValues[2]
     },
     toolbarCloseText: '确定',
-    //onOpen: function (picker) {
+    onOpen: function (picker) {
     //    if (temppk[0]) {
     //        picker.setValue(temppk[0])
     //        temppk[0] = null
     //    }
-    //},
+    picker.setValue(defaultlocation);
+    },
     cols: [{
             width: 160,
             textAlign: 'left',
@@ -404,37 +357,15 @@ function getinfoAjaxSuccess(data) {
     var stu = {
         "stuname": data.recordset.姓名,
         "gender": data.recordset.性别,
-        "dob": dobformpid(data.recordset.身份证件号), // data.recordset.出生日期||dobformpid(data.recordset.身份证件号),
+        "dob": dobformpid(data.recordset.身份证件号),
         "nation": data.recordset.民族,
         "homeaddress": data.recordset.家庭地址,
         "regaddress": data.recordset.现住址,
-        //"origin": data.recordset.籍贯,
         "sid": data.recordset.学籍号,
         "fname": data.recordset.成员1姓名,
         "sname": data.recordset.成员2姓名,
-        "tel": data.recordset.联系电话,
-        //"onlychild": [data.recordset.是否独生子女],
-        //"preschool": [data.recordset.是否受过学前教育],
-        //"regtype": data.recordset.户口性质,
-        //"borncode": data.recordset.出生地行政区划代码,
-        //"regcode": data.recordset.户口所在地行政区划,
-        //"zipcode": data.recordset.邮政编码
+        "tel": data.recordset.联系电话
     }
-    var fs = {
-            //"fname": data.recordset.成员1姓名,
-            //"frelationship": data.recordset.成员1关系,
-            //"Fguardian": [data.recordset.成员1是否监护人],
-            //"fhomeaddress": data.recordset.成员1现住址,
-            //"fregcode": data.recordset.成员1户口所在地行政区划,
-            //"ftel": data.recordset.成员1联系电话,
-            //"sname": data.recordset.成员2姓名,
-            //"srelationship": data.recordset.成员2关系,
-            //"sguardian": [data.recordset.成员2是否监护人],
-            //"shomeaddress": data.recordset.成员2现住址,
-            //"sregcode": data.recordset.成员2户口所在地行政区划,
-            //"stel": data.recordset.成员2联系电话
-            //"tel": data.recordset.联系电话
-        }
         /*
             var pickerVal = stu.pid.substr(0,6)+'000000';
             if (stu.borncode === "" || !stu.borncode){
@@ -462,13 +393,7 @@ function getinfoAjaxSuccess(data) {
                 temppk[3]=getPickerVal(fs.sregcode)
             }
         */
-    myApp.formFromJSON('#form1', stu)
-        //myApp.formFromJSON('#form2', fs)
-
-    //var dob = stu.dob.substr(0, 4) + "-" + stu.dob.substr(5, 2) + "-" + stu.dob.substr(6, 2);
-    //calendarDateFormat.value = [dob]
-    //$('#pid').trigger("blur");
-    //alert(data.recordset.成员2联系电话)
+    myApp.formFromJSON('#form1', stu);
 }
 
 //ajax执行出错时执行
